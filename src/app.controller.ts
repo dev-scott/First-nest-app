aa8f079f60b3c6e1be5626116dbcfa048c6c6992
Dev-scott <sadoscott25@gmail.com>
2024-03-01 13:52:12 +0100

    learn nestJs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,35 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AnswerDto } from './dto/app.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getQueryStrings(@Query('name') username, @Query('age') age): string {
+    return `${username} , ${age}`;
+  }
+
+  @Get('/askquestion')
+  askQuestion(): string {
+    return 'How are you';
+  }
+
+  @Get(':id')
+  getRouteParam(@Param('id') userId): string {
+    return `${userId}`;
+  }
+
+  @Post('/answer')
+  answer(@Body() getAnswerDto: AnswerDto) {
+    return {
+      answer: getAnswerDto.answer,
+    };
   }
 }
